4761b08998113b57819172fbaeb9ef61162234f8
Moisés Man <mcarlosman@gmail.com>
2020-06-03 16:27:01 -0500

    generating bingo card

--- index.js
+++ index.js
@@ -1,56 +1,62 @@
 let NUMBERS = [];
- 
+let B=[]
+let I=[]
+let N=[]
+let G=[]
+let O=[]
+
 // set inital valid values range 1 to 75
 function setNumbers(){
     for (let i=1; i<=75; i++) {
         NUMBERS.push(i)
     }
+
+    B=NUMBERS.slice(0,15)    
+    I=NUMBERS.slice(15,30)    
+    N=NUMBERS.slice(30,45)    
+    G=NUMBERS.slice(45,60)    
+    O=NUMBERS.slice(60)    
 }
 
 
 // return unique random number from pool
-function generateNumber(){
-    const index=Math.floor( Math.random() * NUMBERS.length)  + 1
-    return NUMBERS.splice([index], 1)[0]
+function generateNumber(array){
+    return function() {
+        const index=Math.floor( Math.random() * array.length)  
+        return array.splice([index], 1)[0]
+    }
 }
 
 //generate random values for a bingo card
 function generateCard() {
-    const B=[]    
-    const I=[]    
-    const N=[]    
-    const G=[]    
-    const O=[]    
 
-    
-    for (let i=0; i<25; i++) {
-        const number = generateNumber()
+    const b=[]
+    const i=[]
+    const n=[]
+    const g=[]
+    const o=[]
 
-        if (number>=1 && number <=15){
-            B.push(number)
-        }
-        if (number>=16 && number <=30){
-            I.push(number)
-        }
-        if (number>=31 && number <=45){
-            N.push(number)
-        }
-        if (number>=46 && number <=60){
-            G.push(number)
-        }
-        if (number>=61 && number <=75){
-            O.push(number)
-        }
+    for (let idx=0;idx<5;idx++){
+        b.push( generateBNumber() )  
+        i.push( generateINumber() )  
+        n.push( generateNNumber() )  
+        g.push( generateGNumber() )  
+        o.push( generateONumber() )  
     }
 
-    return [B,I,N,G,O]
+    return [b,i,n,g,o]    
 }
 
 
 //initialize
 setNumbers()
-console.log(NUMBERS)
-console.log(NUMBERS.length)
+
+
+const generateBNumber = generateNumber(B)
+const generateINumber = generateNumber(I)
+const generateNNumber = generateNumber(N)
+const generateGNumber = generateNumber(G)
+const generateONumber = generateNumber(O)
 
 
 console.log( generateCard() )
